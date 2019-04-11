f317559aa1da30279f65f0fe90b617956cd52e49
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-04-11 12:01:07 -0300

    etapa 1 finished
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

---
+++ part1_lexical/tests/test1.c
@@ -0,0 +1,10 @@
+int asd = 123;
+
+while(asd) {
+    float qwe = 1.3E+123;
+    string text = "";
+    string text2 = "oaijsdoijasodj ação";
+    char wad='1';
+    bool t = true;
+    bool f = false;
+}
